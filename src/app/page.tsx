07ba3494ef50a61b3bc6c14163cad2d9cacc9687
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-02-21 16:36:36 -0300

    chore: test ci

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Testing CI/CD
+            Re-Testing CI/CD
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
