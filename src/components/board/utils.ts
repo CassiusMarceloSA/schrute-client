afdb2e1e64ad45f13bd050820a492edfe47466b6
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-03-19 00:21:43 -0300

    refactor: substitute older helper functions

--- src/components/board/utils.ts
+++ src/components/board/utils.ts
@@ -1,46 +1,45 @@
-export const reorder = (list: any, startIndex: number, endIndex: number) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-  return result;
+import { ColumnEnum, Task } from "@/models";
+
+export const getIndicators = (column: ColumnEnum) => {
+  const indicators = Array.from(
+    document.querySelectorAll(`[data-column="${column}"]`)
+  );
+  return indicators as HTMLElement[];
 };
 
-export const move = (
-  source: any,
-  destination: any,
-  droppableSource: any,
-  droppableDestination: any
+export const getNearestIndicator = (
+  e: React.DragEvent<HTMLDivElement>,
+  indicators: HTMLElement[]
 ) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-  const [removed] = sourceClone.splice(droppableSource.index, 1);
+  const DISTANCE_OFFSET = 50;
+  const el = indicators.reduce(
+    (closest, child: HTMLElement) => {
+      const box = child.getBoundingClientRect();
+      const offset = e.clientY - (box.top + DISTANCE_OFFSET);
+      if (offset < 0 && offset > closest.offset) {
+        return { offset, element: child };
+      }
 
-  destClone.splice(droppableDestination.index, 0, removed);
-
-  const result = {} as any;
-  result[droppableSource.droppableId] = sourceClone;
-  result[droppableDestination.droppableId] = destClone;
-
-  return result;
+      return closest;
+    },
+    {
+      offset: Number.NEGATIVE_INFINITY,
+      element: indicators[indicators.length - 1],
+    }
+  );
+  return el;
 };
 
-const grid = 8;
+export const reorder = (list: Task[], card: Task, before: string) => {
+  const copy = list.filter((item) => item.id !== card.id);
+  const moveToBack = before === "-1";
+  if (moveToBack) {
+    copy.push(card);
+  } else {
+    const index = copy.findIndex((card) => card.id === before);
+    if (index === undefined) return list;
+    copy.splice(index, 0, card);
+  }
 
-export const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
-  // some basic styles to make the items look a bit nicer
-  userSelect: "none",
-  padding: grid * 2,
-  margin: `0 0 ${grid}px 0`,
-
-  // change background colour if dragging
-  background: isDragging ? "lightgreen" : "grey",
-
-  // styles we need to apply on draggables
-  ...draggableStyle,
-});
-
-export const getListStyle = (isDraggingOver: boolean) => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
-  padding: grid,
-  width: 250,
-});
+  return copy;
+};
