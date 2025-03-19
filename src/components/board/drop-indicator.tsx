a11e34b7812979d5764d45dcf72c98a8446f3d59
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-03-19 00:23:13 -0300

    feat: enhance style on dragging items

--- src/components/board/drop-indicator.tsx
+++ src/components/board/drop-indicator.tsx
@@ -1,16 +1,31 @@
 import { ColumnEnum } from "@/models";
+import { useEffect, useState } from "react";
 
 type Props = {
   beforeId: string;
   column: ColumnEnum;
+  withHighlight?: boolean;
+  fullHeight?: boolean;
 };
 
-const DropIndicator = (props: Props) => {
+const DropIndicator = ({ withHighlight = true, ...props }: Props) => {
+  const [active, setActive] = useState(false);
+  const height = active || props.fullHeight ? `h-12` : "h-1";
+  const opacity = active && withHighlight ? "opacity-1" : "opacity-0";
+  const borderRadius = active ? "rounded-md" : "rounded-none";
+  useEffect(() => {
+    return () => setActive(false);
+  }, []);
+
   return (
     <div
       data-before={props.beforeId || "-1"}
       data-column={props.column}
-      className="my-0.5 h-0.5 w-full bg-violet-400 opacity-0"
+      onDragEnter={() => setActive(true)}
+      onDragEnd={() => setActive(false)}
+      onDrop={() => setActive(false)}
+      onDragLeave={() => setActive(false)}
+      className={`${height} w-full border-zinc-400 border-dashed border ${borderRadius} ${opacity}`}
     ></div>
   );
 };
--- src/components/board/task.tsx
+++ src/components/board/task.tsx
@@ -1,5 +1,6 @@
 import { Task as TaskType } from "@/models";
 import DropIndicator from "./drop-indicator";
+import { useState } from "react";
 
 type Props = {
   item: TaskType;
@@ -7,13 +8,19 @@ type Props = {
 };
 
 const Card = (props: Props) => {
+  const [dragging, setDragging] = useState(false);
+  const bg = !dragging ? "bg-neutral-800" : "bg-yellow-800";
   return (
     <>
       <DropIndicator beforeId={props.item?.id} column={props.item?.status} />
       <div
         draggable
-        onDragStart={(e) => props.handleDragStart(e, props?.item)}
-        className="cursor-grab rounder border border-neutral-700 bg-neutral-800 p-3 active:cursor-grabbing"
+        onDragStart={(e) => {
+          props.handleDragStart(e, props?.item);
+          setDragging(true);
+        }}
+        onDragEnd={() => setDragging(false)}
+        className={`cursor-grab rounder border border-neutral-700 p-3 active:cursor-grabbing my-1 ${bg}`}
       >
         <p className="text-sm text-neutral-100">{props.item?.title}</p>
       </div>
