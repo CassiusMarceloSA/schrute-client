73c16fddefdb6ab0feb5245ef9a5a0492294a6a7
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-03-26 22:24:28 -0300

    refactor: remove column colors list

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,29 +1,23 @@
 import { Column } from ".";
 // import { Board as BoardType, Task as TaskType } from "@/models";
 import { useBoardStore } from "@/store";
-import { TwTextColor } from "@/utils";
+import { TW_BOARD_COLORS, Color as TwColor } from "@/utils";
 
 // type Props = BoardType;
 
 const Board = () => {
   const { board, setColumn } = useBoardStore();
   const columns = Object.values(board.columns);
-  const COLORS = [
-    "text-orange-500",
-    "text-yellow-500",
-    "text-blue-500",
-    "text-emerald-500",
-  ] satisfies TwTextColor[];
-  
+
   return (
     <div className="h-screen w-full bg-neutral-900 overflow-x-hidden overflow-y-auto text-neutral-50">
-      <div className="flex h-full w-full gap-3 p-12">
+      <div className="flex h-full w-full gap-3 px-12">
         {columns.map((item, index) => (
           <Column
             key={item.id}
             column={item.id}
             title={item.id}
-            headingColor={COLORS[index]}
+            color={TW_BOARD_COLORS[index]}
             cards={item.tasks}
             setColumn={setColumn}
           />
