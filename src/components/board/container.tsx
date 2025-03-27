bb0f529f61ed8439f02db3c21eb54a42584797ec
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-03-27 17:07:15 -0300

    refactor: removed useless component

--- src/components/board/container.tsx
+++
@@ -1,5 +0,0 @@
-import { DragDropContext, DragDropContextProps } from "react-beautiful-dnd";
-
-export function BoardContainer(props: DragDropContextProps) {
-  return <DragDropContext {...props}></DragDropContext>;
-}
--- src/components/board/index.ts
+++ src/components/board/index.ts
@@ -1,4 +1,3 @@
-export { BoardContainer as Container } from "./container";
 export { default as Column } from "./column";
 export { default as Task } from "./task";
 export { default as Board } from "./board";
