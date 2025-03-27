168a99bf69f1cb7561221864a04ace2e4c59f8a5
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-03-27 15:15:04 -0300

    feat: add update status handler to service

--- src/services/task.ts
+++ src/services/task.ts
@@ -10,6 +10,15 @@ export async function getTasks(): Promise<GetTasksResponse> {
   const { data } = await request.get("/tasks");
   return data;
 }
+
+export async function updateTaskStatus(
+  id: string,
+  status: string
+): Promise<Task> {
+  const { data } = await request.put(`/tasks/${id}/status`, { status });
+  return data;
+}
+
 export async function createTask() {}
 export async function updateTask() {}
 export async function deleteTask() {}
