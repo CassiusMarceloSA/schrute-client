59c865ea8b3e8678f04d25038e79f6e2a2b0cf01
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-03-27 14:54:07 -0300

    feat: include validator to update status

--- src/app/api/tasks/validators.ts
+++ src/app/api/tasks/validators.ts
@@ -1,14 +1,24 @@
 import { z } from "zod";
 
+const statusValidator = z.enum(["backlog", "doing", "done", "todo"], {
+  message:
+    "Status is required and should be one of the following: backlog, doing, done, todo",
+});
+
 const createTaskSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   description: z.string().min(1, { message: "Description is required" }),
-  status: z.enum(["backlog", "doing", "done", "todo"], {
-    message:
-      "Status is required and should be one of the following: backlog, doing, done, todo",
-  }),
+  status: statusValidator,
   duration: z.string().min(1, { message: "Duration is required" }),
 });
+
+const updateStatusSchema = z.object({
+  status: statusValidator,
+});
+
+export function validateUpdateStatus(body: Record<string, string>) {
+  return updateStatusSchema.safeParse(body);
+}
 
 export function validateCreateTask(body: Record<string, string>) {
   return createTaskSchema.safeParse(body);
