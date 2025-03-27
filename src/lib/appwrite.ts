a3c0cd74eff00b2954cd4f6ea641f78b7d47e119
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-03-26 22:21:24 -0300

    refactor: using env variables to create appwrite class instance

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,12 +1,18 @@
-import { Client, Databases, Storage} from "appwrite";
+import { Client, Databases, Storage } from "appwrite";
 
-const APPWRITE_URL = "http://localhost/v1";
-const APPWRITE_PROJECT_ID = "xxxxxxxxxxxxxxxxx";
+const APPWRITE_URL = process.env.APPWRITE_URL || "";
+const APPWRITE_PROJECT_ID = process.env.APPWRITE_PROJECT_ID || "";
+const APPWRITE_API_KEY = process.env.APPWRITE_API_KEY || "";
+
+if (!APPWRITE_URL || !APPWRITE_PROJECT_ID || !APPWRITE_API_KEY) {
+  throw new Error("Missing Appwrite environment variables");
+}
 
 const client = new Client();
 
 client.setEndpoint(APPWRITE_URL);
 client.setProject(APPWRITE_PROJECT_ID);
+client.headers["X-Appwrite-Key"] = APPWRITE_API_KEY;
 
 const databases = new Databases(client);
 const storages = new Storage(client);
