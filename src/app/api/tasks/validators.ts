7ab692210eb698250c03c3974e5caae189494ba7
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-03-28 23:05:22 -0300

    fix: fix type of duration attribute

--- src/app/api/tasks/validators.ts
+++ src/app/api/tasks/validators.ts
@@ -9,7 +9,7 @@ const createTaskSchema = z.object({
   title: z.string().min(1, { message: "Title is required" }),
   description: z.string().min(1, { message: "Description is required" }),
   status: statusValidator,
-  duration: z.string().min(1, { message: "Duration is required" }),
+  duration: z.number().min(1, { message: "Duration is required" }),
 });
 
 const updateStatusSchema = z.object({
