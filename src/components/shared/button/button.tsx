717822ad73028dec1f36d91df0f6d463b6c9840c
Cassius de Ávila <cassius_avila@sicredi.com.br>
2025-04-01 00:48:27 -0300

    fix: add default type button to shared component

--- src/components/shared/button/button.tsx
+++ src/components/shared/button/button.tsx
@@ -1,7 +1,7 @@
 import { Button as LibButton, ButtonProps } from "@/components/ui/button";
 
 function Button(props: ButtonProps) {
-  return <LibButton {...props} />;
+  return <LibButton {...props} type={props.type || "button"} />;
 }
 
 export default Button;
