cc54808911410a48ade117c0e0146bf803e49b93
CassiusMarceloSA <cassius.avila@edu.pucrs.br>
2025-04-04 09:14:38 -0300

    refactor: add field component to task form

--- src/components/shared/input/input.tsx
+++ src/components/shared/input/input.tsx
@@ -1,7 +1,10 @@
+import { cn } from "@/lib/utils";
+
 type Props = React.InputHTMLAttributes<HTMLInputElement> & {
   name: string;
   asTextArea?: boolean;
   rows?: number;
+  ref?: React.RefObject<HTMLInputElement | null>;
 };
 
 export const Wrapper = ({ children }: { children: React.ReactNode }) => (
@@ -10,14 +13,22 @@ export const Wrapper = ({ children }: { children: React.ReactNode }) => (
   </div>
 );
 
-export const Container = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex min-h-9 text-neutral-50 w-full rounded-md border border-neutral-700  bg-transparent px-3 py-1 text-base shadow-sm transition-colors">
-    {children}
-  </div>
-);
+export const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  const baseClasses =
+    "flex items-center gap-2 min-h-9 text-neutral-50 w-full rounded-md border border-neutral-700  bg-transparent px-3 py-1 text-base shadow-sm transition-colors";
+
+  return <div className={cn(baseClasses, className)}>{children}</div>;
+};
 
 export const Field = ({
   name,
+  className,
   ...props
 }: React.InputHTMLAttributes<HTMLInputElement> & Props) => {
   const commonClasses =
@@ -26,9 +37,10 @@ export const Field = ({
   if (props.asTextArea) {
     const textAreaProps =
       props as React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+    const textAreaClasses = "resize-none scrollbar-hide";
     return (
       <textarea
-        className={`${commonClasses} resize-none scrollbar-hide`}
+        className={cn(commonClasses, textAreaClasses, className)}
         id={name}
         name={name}
         rows={3}
@@ -38,6 +50,11 @@ export const Field = ({
   }
 
   return (
-    <input className={`${commonClasses}`} id={name} name={name} {...props} />
+    <input
+      className={cn(commonClasses, className)}
+      id={name}
+      name={name}
+      {...props}
+    />
   );
 };
