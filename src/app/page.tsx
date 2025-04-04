1e9551724cc9d4fd4c52a5b7f70030ee015f121f
CassiusMarceloSA <cassius.avila@edu.pucrs.br>
2025-04-04 09:16:10 -0300

    feat: created header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { Board } from "@/components";
+import { Board, Header } from "@/components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 export default function Home() {
+  
+
   return (
-    <div className="h-screen w-screen bg-neutral-900 flex flex-row items-center justify-center font-[family-name:var(--font-geist-sans)]">
+    <div className="h-screen w-screen bg-neutral-900 flex flex-col items-center justify-center font-[family-name:var(--font-geist-sans)]">
       <QueryClientProvider client={queryClient}>
+        <Header />
         <Board />
       </QueryClientProvider>
     </div>
