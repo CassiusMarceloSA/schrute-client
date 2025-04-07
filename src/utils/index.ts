ce60d97aa084ef1c9c33d9b89faa8bc87ac02855
CassiusMarceloSA <cassius.avila@edu.pucrs.br>
2025-04-07 01:07:58 -0300

    feat: include util to format date

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,4 @@ export { TW_BOARD_COLORS } from "./tailwind-colors";
 export type * from "./tailwind-colors";
 export { default as toResult } from "./response";
 export { default as request } from "./request";
+export { default as formatDate } from "./date-formatter";
