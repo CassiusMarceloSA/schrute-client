45d82ce4af84a25cdfb8196ef120bdadfa8b5d8f
CassiusMarceloSA <cassius.avila@edu.pucrs.br>
2025-04-26 04:44:53 -0300

    feat: enhance ImportFileModal with task creation and improved UI for task selection

--- src/components/import-file/modal.tsx
+++ src/components/import-file/modal.tsx
@@ -7,18 +7,24 @@ import FileInput from "./file-input";
 import { useTaskGenerator } from "@/hooks/useTaskGenerator";
 import { Task } from "@/models";
 import { Checkbox } from "../ui/checkbox";
-
+import { useMutation } from "@tanstack/react-query";
+import { taskService } from "@/services";
+import { useFetchTasks } from "@/hooks";
 const ImportFileModal = () => {
   const [open, setOpen] = useState(false);
   const [currentStep, setCurrentStep] = useState(0);
   const [file, setFile] = useState<File | null>(null);
   const [tasks, setTasks] = useState<Task[]>([]);
   const [selectedTasks, setSelectedTasks] = useState<Task[]>([]);
+  const { refetch } = useFetchTasks({
+    enabled: false,
+  });
   const { mutate: generateTasks, isPending } = useTaskGenerator({
     onSuccess: (data: Task[]) => {
       const tasks = data.map((item, index) => ({
         ...item,
         status: "backlog",
+        duration: 30,
         id: index.toString(),
       })) as any;
       setTasks(tasks);
@@ -44,6 +50,18 @@ const ImportFileModal = () => {
       label: "Select tasks to add",
     },
   ];
+
+  const { mutate: createMultipleTasks, isPending: isCreating } = useMutation({
+    mutationFn: (tasks: Task[]) => taskService.createMultipleTasks(tasks),
+    onSuccess: async () => {
+      setOpen(false);
+      setFile(null);
+      setTasks([]);
+      setSelectedTasks([]);
+      setCurrentStep(0);
+      await refetch();
+    },
+  });
 
   return (
     <Modal
@@ -76,36 +94,39 @@ const ImportFileModal = () => {
           />
         )}
         {currentStep === 1 && (
-          <div className="flex flex-col gap-2">
-            <p className="text-sm text-neutral-500">Tasks</p>
-            <ul className="flex flex-col gap-4 overflow-y-auto max-h-[350px] scrollbar-hide">
-              {tasks.map((task) => (
-                <li key={task.id} className="flex items-center gap-2">
-                  <Checkbox
-                    id={task.id}
-                    className="data-[state=checked]:bg-purple-500 data-[state=checked]:border-purple-500 border-neutral-700"
-                    checked={selectedTasks.includes(task)}
-                    onCheckedChange={(checked) => {
-                      handleSelectTask(!!checked, task);
-                    }}
-                  />
-                  <label htmlFor={task.id}>{task.title}</label>
-                </li>
-              ))}
-            </ul>
+          <>
+            <div className="flex flex-col gap-2">
+              <p className="text-sm text-neutral-500">Tasks</p>
+              <ul className="flex flex-col gap-4 overflow-y-auto max-h-[350px] scrollbar-hide">
+                {tasks.map((task) => (
+                  <li key={task.id} className="flex items-center gap-2">
+                    <Checkbox
+                      id={task.id}
+                      className="data-[state=checked]:bg-purple-500 data-[state=checked]:border-purple-500 border-neutral-700"
+                      checked={selectedTasks.includes(task)}
+                      onCheckedChange={(checked) => {
+                        handleSelectTask(!!checked, task);
+                      }}
+                    />
+                    <label htmlFor={task.id}>{task.title}</label>
+                  </li>
+                ))}
+              </ul>
 
-            <p className="text-xs text-end text-neutral-500">
-              You have selected {selectedTasks.length} tasks of {tasks.length}
-            </p>
-          </div>
+              <p className="text-xs text-end text-neutral-500">
+                You have selected {selectedTasks.length} tasks of {tasks.length}
+              </p>
+            </div>
+            <Button
+              variant="secondary"
+              disabled={selectedTasks.length === 0 || isCreating}
+              className="col-span-2 mt-4"
+              onClick={() => createMultipleTasks(selectedTasks)}
+            >
+              {isCreating ? "Adding tasks..." : "Confirm tasks"}
+            </Button>
+          </>
         )}
-        <Button
-          variant="secondary"
-          disabled={selectedTasks.length === 0}
-          className="col-span-2 mt-4"
-        >
-          Confirm tasks
-        </Button>
       </div>
     </Modal>
   );
